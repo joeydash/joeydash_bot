eea37a4c28040a436b039b5270f6501bd5c7e96c
joeydash <joydassudipta@gmail.com>
2018-05-01 00:03:25 +0530

    first commit

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
+import os
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+
 
 app = Flask(__name__)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
-
+joeydash_bot = ChatBot(
+    'JoeydashBot',
+    storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
+    database = "joeydash",
+    database_uri = "mongodb://joeydash:joeydash@ds135790.mlab.com:35790/joeydash",
+    trainer='chatterbot.trainers.ListTrainer'
+)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/train")
+def train():
+    for filename in os.listdir('static/sample'):
+        chats = open('./static/sample/'+filename,'r').readlines()
+        joeydash_bot.train(chats)
+	return render_template("index.html")
+
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    return str(joeydash_bot.get_response(userText))
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3000)
