2a9783ad058565fbb6321900b4d92b1a8e64fa7a
joeydash <joydassudipta@gmail.com>
2018-05-01 20:32:03 +0530

    chatbox changed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import os
+# import os
 from flask import Flask, render_template, request
+from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot import ChatBot
-
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ joeydash_bot = ChatBot(
     storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
     database = "joeydash",
     database_uri = "mongodb://joeydash:joeydash@ds135790.mlab.com:35790/joeydash",
-    trainer='chatterbot.trainers.ListTrainer'
+    filters=["chatterbot.filters.RepetitiveResponseFilter"]
 )
 
 @app.route("/")
@@ -18,18 +18,25 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/train")
-def train():
-    for filename in os.listdir('static/sample'):
-        chats = open('./static/sample/'+filename,'r').readlines()
-        joeydash_bot.train(chats)
-	return render_template("index.html")
+# @app.route("/train")
+# def train():
+    # joeydash_bot.set_trainer(ChatterBotCorpusTrainer)
+    # joeydash_bot.train(
+    #     "chatterbot.corpus.english.conversations"
+    # )
+    # joeydash_bot.set_trainer(chatterbot.trainers.ListTrainer)
+    # for filename in os.listdir('static/sample'):
+    #     chats = open('./static/sample/'+filename,'r').readlines()
+    #     joeydash_bot.train(chats)
+    # return render_template("index.html")
 
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return str(joeydash_bot.get_response(userText))
-
+    try:
+        return str(joeydash_bot.get_response(userText))
+    except Exception:
+        return str("I didn't get you")
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000)
